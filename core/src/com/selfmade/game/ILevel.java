927f57de23b65bf342d7a9cb87711b586918b10d
AlexResmyatov <alex.resmyatov@gmail.com>
2015-08-27 17:51:00 +0600

    sdg

---
+++ core/src/com/selfmade/game/ILevel.java
@@ -0,0 +1,11 @@
+package com.selfmade.game;
+
+import java.util.List;
+
+import com.selfmade.objects.IGameObject;
+
+public interface ILevel {	
+	
+	public List<IGameObject> getAllObjects();
+	
+}
---
+++ core/src/com/selfmade/objects/IGameObject.java
@@ -0,0 +1,5 @@
+package com.selfmade.objects;
+
+public class IGameObject {
+
+}
